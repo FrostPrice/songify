d706e840bf06ad720a82fa0e9606e245abee9a16
mateusbarbosa <barbosamat16@gmail.com>
2023-12-03 19:52:55 -0300

    Fix column names and add options column in Genre, Musics, Artists, and Albums views

--- backend/src/controllers/album.controller.js
+++ backend/src/controllers/album.controller.js
@@ -9,60 +9,64 @@ const Op = db.Sequelize.Op;
 // INSERT INTO "Albums" ("id","name","duration","releaseDate","createdAt","updatedAt") VALUES (DEFAULT,'name','duration','releaseDate','createdAt','updatedAt');
 // INSERT INTO "artistas_album" ("id","createdAt","updatedAt","ArtistaId","AlbumId") VALUES (DEFAULT,'createdAt','updatedAt','ArtistaId','AlbumId');
 // INSERT INTO "album_musicas" ("id","createdAt","updatedAt","AlbumId","MusicaId") VALUES (DEFAULT,'createdAt','updatedAt','AlbumId','MusicaId');
-exports.insert = (req, res) => {
-  Album.create({
-    name: req.body.name,
-    duration: req.body.duration,
-    releaseDate: req.body.releaseDate,
-  })
-    .then((album) => {
-      if (req.body.artistName) {
-        Artist.findAll({
-          where: {
-            name: {
-              [Op.or]: req.body.artistName,
-            },
-          },
-        }).then((artist) => {
-          album.setArtistas(artist).catch((err) => {
-            res.status(404).send({ message: "Artist not found" });
-          });
-        });
-      }
-
-      if (!req.body.musics) {
-        res.send({ message: "Album registered successfully!" });
-      } else {
-        Music.findAll({
-          where: {
-            name: {
-              [Op.or]: req.body.musics,
-            },
-          },
-        })
-          .then((musicsFound) => {
-            musicsFound.forEach((music) => {
-              album.setMusicas(music).catch((err) => {
-                res.status(404).send({ message: "Music not found" });
-              });
-            });
-
-            res.send({ message: "Album registered successfully!" });
-          })
-          .catch((err) => {
-            res.status(404).send({ message: "Musics not found" });
-          });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({ message: err.message });
+exports.insert = async (req, res) => {
+  try {
+    const album = await Album.create({
+      name: req.body.name,
+      duration: req.body.duration,
+      releaseDate: req.body.releaseDate,
     });
+
+    if (req.body.artistName) {
+      const artist = await Artist.findAll({
+        where: {
+          name: {
+            [Op.or]: req.body.artistName,
+          },
+        },
+      });
+
+      if (!artist) {
+        return res.status(404).send({ message: "Artist not found" });
+      }
+
+      await album.setArtista(artist);
+    }
+
+    if (req.body.musics) {
+      let musicsFound = [];
+
+      for (let musicName of req.body.musics) {
+        const music = await Music.findOne({
+          where: {
+            name: musicName,
+          },
+        });
+
+        if (music) musicsFound.push(music);
+      }
+
+      if (!musicsFound || musicsFound.length === 0) {
+        return res
+          .status(202)
+          .send({ message: "Album was created, but with no music" });
+      }
+
+      for (let music of musicsFound) {
+        await album.setMusicas(music);
+      }
+    }
+
+    return res.send({ message: "Album registered successfully!" });
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
 };
 
 // * SQL Script: SELECT
 // SELECT "Albums"."id", "Albums"."name", "Albums"."duration", "Albums"."releaseDate", "Artistas"."name" AS "Artistas.name", "Artistas"."id" AS "Artistas.id", "Artistas"."createdAt" AS "Artistas.createdAt", "Artistas"."updatedAt" AS "Artistas.updatedAt" FROM "Albums" AS "Albums" LEFT OUTER JOIN ("artistas_album" AS "ArtistaAlbum" INNER JOIN "Artistas" AS "Artistas" ON "ArtistaAlbum"."ArtistaId" = "Artistas"."id") ON "Albums"."id" = "ArtistaAlbum"."AlbumId";
 exports.get = (req, res) => {
-  Album.findAll()
+  Album.findAll({ include: [{ model: Artist, through: "artistas_album" }] })
     .then((albums) => {
       res.send(albums);
     })
@@ -75,62 +79,68 @@ exports.get = (req, res) => {
 // UPDATE "Albums" SET "name"='name','duration'='duration','releaseDate'='releaseDate','createdAt'='createdAt','updatedAt'='updatedAt' WHERE "id" = id;
 // UPDATE "artistas_album" SET "createdAt"='createdAt','updatedAt'='updatedAt','ArtistaId'='ArtistaId','AlbumId'='AlbumId' WHERE "id" = id;
 // UPDATE "album_musicas" SET "createdAt"='createdAt','updatedAt'='updatedAt','AlbumId'='AlbumId','MusicaId'='MusicaId' WHERE "id" = id;
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   const id = req.params.id;
 
-  Album.update(req.body, {
-    where: { id: id },
-  })
-    .then((album) => {
-      if (req.body.artistName) {
-        Artist.findAll({
-          where: {
-            name: {
-              [Op.or]: req.body.artistName,
-            },
-          },
-        }).then((artist) => {
-          album.setArtistas(artist).catch((err) => {
-            res.status(404).send({ message: "Artist not found" });
-          });
-        });
-      }
-
-      if (!req.body.musics) {
-        if (album == 1) {
-          res.send({ message: "Album was updated successfully." });
-        } else {
-          res.send({ message: `Cannot update Album with id=${id}.` });
-        }
-      } else {
-        Music.findAll({
-          where: {
-            name: {
-              [Op.or]: req.body.musics,
-            },
-          },
-        })
-          .then((musicsFound) => {
-            musicsFound.forEach((music) => {
-              album.setMusicas(music).catch((err) => {
-                res.status(404).send({ message: "Music not found" });
-              });
-            });
-
-            if (album == 1) {
-              res.send({ message: "Album was updated successfully." });
-            } else {
-              res.send({ message: `Cannot update Album with id=${id}.` });
-            }
-          })
-          .catch((err) => {
-            res.status(404).send({ message: "Musics not found" });
-          });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({ message: err.message });
+  try {
+    const updatAlbum = await Album.update(req.body, {
+      where: { id: id },
+      include: [
+        { model: Artist, through: "artistas_album" },
+        { model: Music, through: "album_musicas" },
+      ],
     });
+
+    if (!updatAlbum) {
+      return res.send({ message: `Cannot update Album with id=${id}.` });
+    }
+
+    const album = await Album.findByPk(id);
+
+    if (req.body.artistName) {
+      const artist = await Artist.findOne({
+        where: {
+          name: req.body.artistName,
+        },
+      });
+
+      if (!artist) {
+        return res.status(404).send({ message: "Artist not found" });
+      }
+
+      console.log(album);
+
+      await album.setArtista(artist);
+    }
+
+    if (req.body.musics) {
+      let musicsFound = [];
+
+      for (let musicName of req.body.musics) {
+        const music = await Music.findOne({
+          where: {
+            name: musicName,
+          },
+        });
+
+        if (music) musicsFound.push(music);
+      }
+
+      if (!musicsFound || musicsFound.length === 0) {
+        return res
+          .status(200)
+          .send({ message: "Album was updated, but with no music" });
+      }
+
+      for (let music of musicsFound) {
+        await album.setMusicas(music);
+      }
+    }
+
+    return res.send({ message: "Album was updated successfully." });
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
 };
 
 // * SQL Script: DELETE
